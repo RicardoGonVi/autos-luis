09b20057b2169796f1ca8bd52ba1d83c67f0a48f
RicardoGonVi <ricardogonzalez.11.99@gmail.com>
2025-09-19 16:46:21 -0600

    Create search car function

--- app/pages/search/search.py
+++ app/pages/search/search.py
@@ -2,14 +2,21 @@ import streamlit as st
 
 from pages.search.cars import *
 
-# Search page main content
-st.markdown("# Buscar 🔍")
-st.sidebar.markdown("# Buscar 🔍")
 
-st.markdown('## Vehículos 🚗')
+def search_car():
+    data_filter = CarFilter()
+    data = load_database()
+    get_filter(data, data_filter)
+    filtered_data = apply_filter(data, data_filter)
+    st.dataframe(filtered_data)
 
-data_filter = CarFilter()
-data = load_database()
-get_filter(data, data_filter)
-filtered_data = apply_filter(data, data_filter)
-st.dataframe(filtered_data)
+
+def main():
+    st.markdown("# Buscar 🔍")
+    st.sidebar.markdown("# Buscar 🔍")
+
+    st.markdown('## Vehículos 🚗')
+    search_car()
+
+
+main()
